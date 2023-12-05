b923a57e7b6aa257dd756fce1692b5644249de1b
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-05 13:57:38 -0500

    add route use handler

--- backend/routes/v1/index.ts
+++ backend/routes/v1/index.ts
@@ -1,8 +1,25 @@
 import express from 'express';
-import base from './base.routes';
+import config from '../../config/config';
+import baseRoute from './base.routes';
 
 const router = express.Router();
 
-router.use('/base', base);
+type Branch = { path: string; route: express.Router };
+type Branches = Branch[];
+const defaultRoutes: Branches = [{ path: '/base', route: baseRoute }];
+
+/* routes available only in development mode */
+const devRoutes: Branches = [];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
 
 export default router;
