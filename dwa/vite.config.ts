b0b8cca44f2dbe308691d715c09ab6f42d42edbf
Andrew Glago <andrewglago1@gmail.com>
2023-12-06 19:58:41 +0000

    feat: add shadcn/ui

--- dwa/vite.config.ts
+++ dwa/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()]
+  plugins: [react(), VitePWA()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
