d2c9dc372ea2d4aa5951a897d96f9336147cb20d
Andrew Glago <andrewglago1@gmail.com>
2023-12-06 20:02:54 +0000

    Revert "feat: add shadcn/ui"
    This reverts commit b0b8cca44f2dbe308691d715c09ab6f42d42edbf.

--- dwa/vite.config.ts
+++ dwa/vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), VitePWA()]
 });
