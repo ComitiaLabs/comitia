07c9c758760a50e843e8b6c5dc88e2b1ca0127cb
Andrew Glago <andrewglago1@gmail.com>
2023-12-08 22:53:13 +0000

    fix: use single protocol

--- backend/controller/protocol.controller.ts
+++ backend/controller/protocol.controller.ts
@@ -1,10 +1,10 @@
 import asyncHandler from 'express-async-handler';
 
-import { protocolDefinitions } from '../services/protocols';
+import { protocolDefinition } from '../services/protocols';
 
 /**
  * Returns the required protocols for the application in JSON.
  */
 export const getProtocols = asyncHandler(async (_, res) => {
-  res.status(200).json(protocolDefinitions);
+  res.status(200).json(protocolDefinition);
 });
--- backend/services/protocols.ts
+++ backend/services/protocols.ts
@@ -1,51 +1,50 @@
+import Web5Service from './web5';
 import type { ProtocolDefinition } from '@tbd54566975/dwn-sdk-js';
 
 /**
- * Protocol definitions for the data we store in User DWNs
+ * Protocol defining structure of the data we store in User DWNs
  */
-export const protocolDefinitions = [
-  {
-    protocol: 'https://comitia-help.com/protocol',
-    published: true,
-    types: {
-      message_records: {
-        schema: 'https://comitia-help.com/protocol/chat_records',
-        dataFormats: ['application/json']
-      },
-      health_records: {
-        schema: 'https://comitia-help.com/protocol/health_records',
-        dataFormats: ['application/json']
-      },
-      context: {
-        schema: 'https://comitia-help.com/protocol/context',
-        dataFormats: ['text/plain']
-      }
+export const protocolDefinition = {
+  protocol: 'https://comitia-help.com/protocol',
+  published: true,
+  types: {
+    message_records: {
+      schema: 'https://comitia-help.com/protocol/chat_records',
+      dataFormats: ['application/json']
     },
-    structure: {
-      message_records: {
-        $actions: [
-          { who: 'author', of: 'message_records', can: 'read' },
-          { who: 'author', of: 'message_records', can: 'write' },
-          { who: 'recipient', of: 'message_records', can: 'read' },
-          { who: 'recipient', of: 'message_records', can: 'write' }
-        ]
-      },
-      health_records: {
-        $actions: [
-          { who: 'author', of: 'health_records', can: 'read' },
-          { who: 'author', of: 'health_records', can: 'write' },
-          { who: 'recipient', of: 'health_records', can: 'read' },
-          { who: 'recipient', of: 'health_records', can: 'write' }
-        ]
-      },
-      context: {
-        $actions: [
-          { who: 'author', of: 'context', can: 'read' },
-          { who: 'author', of: 'context', can: 'write' },
-          { who: 'recipient', of: 'context', can: 'read' },
-          { who: 'recipient', of: 'context', can: 'write' }
-        ]
-      }
+    health_records: {
+      schema: 'https://comitia-help.com/protocol/health_records',
+      dataFormats: ['application/json']
+    },
+    context: {
+      schema: 'https://comitia-help.com/protocol/context',
+      dataFormats: ['text/plain']
+    }
+  },
+  structure: {
+    message_records: {
+      $actions: [
+        { who: 'author', of: 'message_records', can: 'read' },
+        { who: 'author', of: 'message_records', can: 'write' },
+        { who: 'recipient', of: 'message_records', can: 'read' },
+        { who: 'recipient', of: 'message_records', can: 'write' }
+      ]
+    },
+    health_records: {
+      $actions: [
+        { who: 'author', of: 'health_records', can: 'read' },
+        { who: 'author', of: 'health_records', can: 'write' },
+        { who: 'recipient', of: 'health_records', can: 'read' },
+        { who: 'recipient', of: 'health_records', can: 'write' }
+      ]
+    },
+    context: {
+      $actions: [
+        { who: 'author', of: 'context', can: 'read' },
+        { who: 'author', of: 'context', can: 'write' },
+        { who: 'recipient', of: 'context', can: 'read' },
+        { who: 'recipient', of: 'context', can: 'write' }
+      ]
     }
   }
-] satisfies ProtocolDefinition[];
+} satisfies ProtocolDefinition;
