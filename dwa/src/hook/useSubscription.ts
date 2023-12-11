8b9cbc7aa710ed048a1863ca1dd1eb5196dfa513
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-10 22:10:11 -0500

    create a subscription hook

---
+++ dwa/src/hook/useSubscription.ts
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+const DID =
+  'did:ion:EiA4OkU_l-Uh4x5DIlJNm4vn-CJGamggG392zIHIEaAf7w:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoielpndXJWTS1qZy1jcEptN3A3QWNsak10UHhFUVJkY0hmR2VKaXNxWm9vUSJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifSx7ImlkIjoiZHduLWVuYyIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJiRC1mSlpPV2ZEZGNuU1gzUW0yOXZXanFTcE1zUElSRHB4R0VMcGhNNDZNIiwieSI6IkJEVVpGaGVmdklIWEFJV1kxZkZCX0xTRDNWb3Q5LWs3MDI3NHFoMFZzakUifSwicHVycG9zZXMiOlsia2V5QWdyZWVtZW50Il0sInR5cGUiOiJKc29uV2ViS2V5MjAyMCJ9XSwic2VydmljZXMiOlt7ImlkIjoiZHduIiwic2VydmljZUVuZHBvaW50Ijp7ImVuY3J5cHRpb25LZXlzIjpbIiNkd24tZW5jIl0sIm5vZGVzIjpbImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNCIsImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduMiJdLCJzaWduaW5nS2V5cyI6WyIjZHduLXNpZyJdfSwidHlwZSI6IkRlY2VudHJhbGl6ZWRXZWJOb2RlIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlBSDVfRE1EaG9LWTZHZVdrTkFKTUM0VXhzSFpoZFR5SVpEbnNydktENWIxUSJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpREwzNFVHN2hadE5vYlIwT1h4S0dPQkFSWC1Ta1hqbHJRNUFrMmZ3clRJcVEiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUQ1aVh3aWNYNE96NE1PbFk5NHVFMmgzTlVrdjh3UU1fSHRWUG5ZcktIUS1RIn19';
+
+const socketUrl = 'localhost:3000';
+
+const useSubscription = () => {
+  const socket = io(socketUrl, {
+    auth: {
+      did: DID,
+    },
+    transports: ['websocket'],
+  });
+
+  useEffect(() => {
+    socket.on('connection', function () {
+      console.log('client connected');
+    });
+
+    socket.on('connect_error', function (err) {
+      console.log('client connect_error: ', err);
+    });
+
+    socket.on('connect_timeout', function (err) {
+      console.log('client connect_timeout: ', err);
+    });
+
+    socket.on('error', function (err: Error) {
+      console.error('server error: ', err);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('connect_error');
+      socket.off('connect_timeout');
+      socket.off('error');
+    };
+  });
+
+  return { socket };
+};
+
+export default useSubscription;
