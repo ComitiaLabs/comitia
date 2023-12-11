8d110e8e3cf7d2382ca9494c8e2f44e9c3dd009d
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-10 21:09:27 -0500

    truncate logged DID

--- backend/services/protocols.ts
+++ backend/services/protocols.ts
@@ -1,5 +1,6 @@
-import Web5Service from './web5';
 import type { ProtocolDefinition } from '@tbd54566975/dwn-sdk-js';
+import logger from '../config/logger';
+import Web5Service from './web5';
 
 /**
  * Protocol defining structure of the data we store in User DWNs
@@ -59,7 +60,7 @@ export async function validateDIDHasProtocol(did: string) {
     throw new Error('Web5 service not initialized');
   }
 
-  console.log('Validating DID has protocol', did);
+  logger.info('Validating DID has protocol', did.slice(0, 20));
 
   try {
     const response = await web5.dwn.protocols.query({
@@ -73,7 +74,7 @@ export async function validateDIDHasProtocol(did: string) {
 
     return response.protocols.length > 0;
   } catch (error) {
-    console.error('DID Protocol Install Validation Error:', error);
+    logger.error('DID Protocol Install Validation Error:', error);
     // This is most likely not an internal error, but rather a validation error
     return false;
   }
