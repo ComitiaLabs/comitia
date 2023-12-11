9bf4ec5e67c4d630472925227fcefff927fdabf0
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-11 14:52:58 -0500

    restyled side bar

--- dwa/src/pages/chat/index.tsx
+++ dwa/src/pages/chat/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import useGetChatList from '@/hook/useGetChatList';
 import { validPath } from '@/lib/routing';
 import { paths } from '@/router';
+import { CircleUserRound, PenSquare } from 'lucide-react';
 import { Link, useOutlet, useParams } from 'react-router-dom';
 
 const Menu = () => {
@@ -9,24 +10,35 @@ const Menu = () => {
   const { id: currentPage } = useParams();
 
   return (
-    <div className="p-3 bg-blue-200 flex flex-col items-stretch gap-2">
-      {data?.map(item => {
-        const isSelected = item.id === currentPage;
+    <div className="p-3 bg-gray-200 grid grid-rows-[min-content_1fr_min-content] flex-col items-stretch gap-2">
+      <Button className="w-full flex justify-between" disabled variant="ghost">
+        Create New Chat
+        <PenSquare className="w-4 h-4" />
+      </Button>
 
-        return (
-          <Link
-            key={item.id}
-            to={validPath(paths.chat_with_id, { id: item.id })}
-          >
-            <Button
-              className="w-full"
-              variant={isSelected ? 'default' : 'secondary'}
+      <div>
+        {data?.map(item => {
+          const isSelected = item.id === currentPage;
+          return (
+            <Link
+              key={item.id}
+              to={validPath(paths.chat_with_id, { id: item.id })}
             >
-              {item.name}
-            </Button>
-          </Link>
-        );
-      })}
+              <Button
+                className="w-full"
+                variant={isSelected ? 'default' : 'secondary'}
+              >
+                {item.name}
+              </Button>
+            </Link>
+          );
+        })}
+      </div>
+
+      <Button className="w-full flex justify-between" disabled variant="ghost">
+        <CircleUserRound className="w-4 h-4" />
+        Your Profile
+      </Button>
     </div>
   );
 };
@@ -35,11 +47,11 @@ const Chat = () => {
   const outlet = useOutlet();
 
   return (
-    <div className="h-full grid grid-cols-5">
+    <div className="h-full grid grid-cols-8">
       <Menu />
 
-      <div className="col-span-4 p-3 h-full flex flex-col justify-between">
-        {outlet || 'Please Select a chat'}
+      <div className="col-span-full col-start-2  p-3 h-full flex flex-col justify-between">
+        {outlet || 'Please select a chat from the sidebar'}
       </div>
     </div>
   );
