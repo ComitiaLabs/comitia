d67f31ee0783102a30eb4b97fb5fee864d6607df
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-12 10:43:52 -0500

    added title for the page

--- dwa/src/App.tsx
+++ dwa/src/App.tsx
@@ -1,11 +1,15 @@
 import { Toaster } from '@/components/ui/toaster';
 import router from '@/router';
+import { Helmet } from 'react-helmet';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Comitia</title>
+      </Helmet>
       <RouterProvider router={router} />
       <Toaster />
     </>
