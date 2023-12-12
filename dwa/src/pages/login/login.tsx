4d6a277f2e7df07fe8c1efffda064915d300eda2
Aikins Dwamena <aikinsakenten@gmail.com>
2023-12-12 01:24:12 +0000

    fixed bugs

--- dwa/src/pages/login/login.tsx
+++ dwa/src/pages/login/login.tsx
@@ -1,78 +1,69 @@
-import { useState, useEffect } from "react";
-import { Web5 } from "@web5/";
-import type { ProtocolDefinition } from '@tbd54566975/dwn-sdk-js';
+import {useState, useEffect} from "react";
+import {Web5} from "@web5/api";
+import type {ProtocolDefinition} from '@tbd54566975/dwn-sdk-js';
+import {env} from "../../env.ts"
 
 const Login = () => {
     const [did, setDID] = useState("");
     const [protocolDefinition, setProtocol] = useState<ProtocolDefinition | null>(null);
-    const [web5Instance, setWeb5Instance] = useState(null);
 
     useEffect(() => {
-        const getWeb5Instance = async () => {
-            try{
-                const { web5 } = await Web5.connect({connectedDID:did});
-                if(!web5Instance){
-                    throw new Error("Failed to get web5 instance");
-                }
-                setWeb5Instance(web5);
-            } catch(error) {
-                console.error("Error getting web5 instance", error)
-            }
-           
-        }
         const fetchProtocol = async () => {
             try {
-                const response = await fetch ("https://comitia-help.com/protocol");
-                if(!response.ok) {
+                const response = await fetch(`${env.VITE_WS_URL}/protocols`);
+                if (!response.ok) {
                     throw new Error("Failed to fetch protocol");
                 }
                 const data = await response.json();
                 setProtocol(data);
-            } catch(error) {
+            } catch (error) {
                 console.error("Error fetching protocol", error);
             }
         };
         fetchProtocol();
-        getWeb5Instance();
     }, [])
+    const getWeb5Instance = async () => {
+        const {web5} = await Web5.connect({connectedDid: did});
+        if (!web5) {
+            throw new Error("Failed to get web5 instance");
+        }
+        return web5;
+    }
 
-    const handleDIDChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const handleDIDChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setDID(e.target.value);
     }
 
-    async function validateDIDHasProtocol(did: string, web5:Web5) {
-      
-        if (!web5) {
-          throw new Error('Web5 service not initialized');
-        }
-      
-        console.log('Validating DID has protocol', did);
-      
-        try {
-          const response = await web5.dwn.protocols.query({
-            from: did,
-            message: {
-              filter: {
-                protocol: 'https://comitia-help.com/protocol'
-              }
-            }
-          });
-      
-          return response.protocols.length > 0;
-        } catch (error) {
-          console.error('DID Protocol Install Validation Error:', error);
-          // This is most likely not an internal error, but rather a validation error
-          return false;
-        }
-      }
+    async function validateDIDHasProtocol(web5: Web5 | null) {
 
-    const installProtocols = async (web5: Web5, protocolDefinition: ProtocolDefinition | null) => {
-        //validate if DID has protocols installed
-        // const hasProtocol = await validateDIDHasProtocol(did,web5Instance);
-        
-        // if(!hasProtocol) {
-        //     return 
-        // }
+        if (!web5) {
+            throw new Error('Web5 service not initialized');
+        }
+
+        try {
+            console.log(protocolDefinition?.protocol)
+            const response = await web5.dwn.protocols.query({
+                message: {
+                    filter: {
+                        protocol: `${protocolDefinition?.protocol}`
+                    }
+                }
+            });
+
+            return response.protocols.length > 0;
+        } catch (error) {
+            console.error('DID Protocol Install Validation Error:', error);
+            // This is most likely not an internal error, but rather a validation error
+            return false;
+        }
+    }
+
+    const installProtocols = async (web5: Web5, protocolDefinition: ProtocolDefinition) => {
+        const hasProtocol = await validateDIDHasProtocol(web5);
+        console.log(hasProtocol);
+        if (hasProtocol) {
+            return
+        }
         return await web5.dwn.protocols.configure({
             message: {
                 definition: protocolDefinition
@@ -81,10 +72,21 @@ const Login = () => {
 
     }
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        installProtocols(web5Instance, protocolDefinition);
+        try {
+            const web5 = await getWeb5Instance();
+            console.log(web5);
+            console.log("handle submit works")
+            if (!protocolDefinition) {
+                throw new Error("Could not find protocol definition");
+            }
+            await installProtocols(web5, protocolDefinition);
 
+        } catch (error) {
+            console.error("Could not get web5 instance", error);
+            return
+        }
 
     }
     return (
