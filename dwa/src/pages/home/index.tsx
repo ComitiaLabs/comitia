526d083f122c8c9d362eae7d128c6f61c1118008
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-12 10:44:55 -0500

    corrected the src

--- dwa/src/pages/home/index.tsx
+++ dwa/src/pages/home/index.tsx
@@ -127,8 +127,8 @@ const Home = () => {
           <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
             <img
               className="w-[48rem] max-w-none rounded-xl bg-primary-foreground shadow-xl ring-1 ring-secondary/10 sm:w-[57rem]"
-              src="/public/app-preview.png"
-              alt=""
+              src="/app-preview.png"
+              alt="A preview of the application"
             />
           </div>
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
