936bffd5d21c6cd9030431a961fe3bec6ed29aec
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-14 08:15:57 +0100

    updated meta

--- dwa/src/App.tsx
+++ dwa/src/App.tsx
@@ -18,26 +18,34 @@ function App() {
         <meta name="revisit-after" content="10 days" />
         <meta name="author" content="ComitiaLabs(https://github.com/ComitiaLabs/)" />
 
-        {/* <!--  Essential META Tags --> */}
-        <meta property="og:title" content="Comitia Help" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://comitia-dwa-production.up.railway.app/app-preview.png" />
+        {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://comitia-dwa-production.up.railway.app" />
-        <meta property="og:description" content="Bot that acts as a therapist. Users get to own their data. All the chats had with this bot and the medical info shared in the conversation are theirs." />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Comitia" />
         <meta property="og:site_name" content="Comitia Help" />
+        <meta property="og:description" content="Bot that acts as a therapist. Users get to own their data. All the chats had with this bot and the medical info shared in the conversation are theirs." />
+        <meta property="og:image" content="https://comitia-dwa-production.up.railway.app/app-preview.png" />
         <meta property="og:locale" content="en_GB" />
+
+        {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="https://comitia-dwa-production.up.railway.app" />
+        <meta property="twitter:url" content="https://comitia-dwa-production.up.railway.app" />
+        <meta name="twitter:title" content="Comitia" />
+        <meta name="twitter:description" content="Bot that acts as a therapist. Users get to own their data. All the chats had with this bot and the medical info shared in the conversation are theirs." />
+        <meta name="twitter:image" content="https://comitia-dwa-production.up.railway.app/app-preview.png" />
 
         {/* <!--  Non-Essential, But Recommended --> */}
-        <meta name="twitter:image:alt" content="Comitia Chat" />
+        <meta name="twitter:image:alt" content="Comitia Help" />
 
         {/* <!--  Favicons --> */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-
       </Helmet>
+
       <RouterProvider router={router} />
+
       <Toaster />
     </HelmetProvider>
   );
