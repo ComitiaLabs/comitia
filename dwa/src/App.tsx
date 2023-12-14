7c177367b3a13a91fb2c7492beee0591016df2d1
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-14 07:37:21 +0100

    added tags

--- dwa/src/App.tsx
+++ dwa/src/App.tsx
@@ -9,6 +9,28 @@ function App() {
     <HelmetProvider>
       <Helmet>
         <title>Comitia</title>
+        <meta name="title" content="Comitia Help" />
+        <meta name="description" content="Bot that acts as a therapist. Users get to own their data. All the chats had with this bot and the medical info shared in the conversation are theirs." />
+        <meta name="keywords" content="mental health, mental, health, AI, web5, TBD, hackathon, replicate" />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="10 days" />
+        <meta name="author" content="ComitiaLabs(https://github.com/ComitiaLabs/)" />
+
+        {/* <!--  Essential META Tags --> */}
+        <meta property="og:title" content="Comitia Help" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://comitia-dwa-production.up.railway.app/app-preview.png" />
+        <meta property="og:url" content="https://comitia-dwa-production.up.railway.app" />
+        <meta property="og:description" content="Bot that acts as a therapist. Users get to own their data. All the chats had with this bot and the medical info shared in the conversation are theirs." />
+        <meta property="og:site_name" content="Comitia Help" />
+        <meta property="og:locale" content="en_GB" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* <!--  Non-Essential, But Recommended --> */}
+        <meta name="twitter:image:alt" content="Comitia Chat" />
       </Helmet>
       <RouterProvider router={router} />
       <Toaster />
