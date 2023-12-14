6dd6bd9357b90b1d8e5d9c7d4cb77bc6d35d4d3f
Obi Fortune <gabrielobi.of@gmail.com>
2023-12-13 19:17:49 -0500

    update config

--- dwa/vite.config.ts
+++ dwa/vite.config.ts
@@ -1,38 +1,42 @@
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import  nodePolyfills from "vite-plugin-node-stdlib-browser"
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    target: ['chrome109', 'edge112', 'firefox102', 'safari15.6', 'ios15.6']
-  },
-  define: {
-    global: 'globalThis'
-  },
-  plugins: [react(), VitePWA(
-      {
+export default defineConfig(({ command }) => {
+  return {
+    build: {
+      target: ['chrome109', 'edge112', 'firefox102', 'safari15.6', 'ios15.6']
+    },
+    define: {
+      global: 'globalThis'
+    },
+    plugins: [
+      react(),
+      VitePWA({
         registerType: 'autoUpdate',
         manifest: {
           icons: [
             {
-              src: "/comitia-logo.jpg",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "any maskable"
+              src: '/comitia-logo.jpg',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
             }
           ]
         },
         devOptions: {
-          enabled: true
+          enabled: command === 'serve' ? true : false
         }
+      }),
+      nodePolyfills()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
       }
-  ), nodePolyfills()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+    }
+  };
 });
